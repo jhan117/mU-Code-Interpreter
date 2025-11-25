79101a1b2f470253f23910fc926d33339712d2b9
rkdtjr <dlrkdtjr0116@kumoh.ac.kr>
2025-11-26 03:06:34 +0900

    refactor: remove unnecessary code

--- include/core/instruction.h
+++ include/core/instruction.h
@@ -21,7 +21,3 @@ const OpInfo *findOpInfoByOpcode(int opcode);
 int encodeInst(int opcode, int operand_val);
 int patchInst(int old_opcode, int new_addr);
 void decodeInst(int old_inst, int *op_group, int *op_group_idx, int *operand);
-int getOperand(int inst);
-int getOpcodeFromGroup(int group_code, int opcode_index);
-int getOpcodeFromInst(int inst);
-const char *getOpcodeName(int opcode);
